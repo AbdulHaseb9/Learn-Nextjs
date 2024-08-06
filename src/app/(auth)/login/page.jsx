67e8c81e63acb1263f0b1b4b09c401c9cb57812f
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2024-08-05 22:27:34 -0700

    pages route groups and nested layouts

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,13 @@
+export default function Login() {
+    return (
+        <div>
+            Here is Login Page
+        </div>
+    )
+}
+
+
+
+// If you want to arrange multiple pages into one folder without changes in url you can give folder name in paranthesis like (auth) it will display your login or register route after hiting home page
+
+// http://localhost:3000/register
